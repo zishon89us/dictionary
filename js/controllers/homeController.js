c203be363afc174609784ade8fa5520e69aafbae
kashifahmed <kashifmind@gmail.com>
2014-07-05 21:09:19 +0500

    Adding some words.

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,17 +1,8 @@
 /**
  * Created by kashif on 7/4/14.
  */
-dictionary.controller("homeController",function($scope){
+dictionary.controller("homeController",function($scope,dataDictionary){
 
-   $scope.datas = [
-       "a","b","c",
-       "d","e","f",
-       "g","h","i",
-       "j","k","l",
-       "m","n","o",
-       "p","q","r",
-       "s","t","u",
-       "w","x","y","z"
-   ];
+   $scope.datas = dataDictionary;
 
 });
